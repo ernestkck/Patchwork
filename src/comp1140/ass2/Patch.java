c1e2aae4749c8269c95d1a4a6c0ca430383d64c8
Jack de Kleuver <u5740954@anu.edu.au>
2017-08-25 21:49:57 +1000

    changed from Boolean to boolean arrays in Patch

--- src/comp1140/ass2/Patch.java
+++ src/comp1140/ass2/Patch.java
@@ -4,186 +4,186 @@ package comp1140.ass2;
  * Created by jack1 on 18/08/2017.
  */
 public enum Patch {
-        A(2, 1, 0, new Boolean[][]{
+        A(2, 1, 0, new boolean[][]{
                 {true},
                 {true}
         }),
-        B(1, 3, 0, new Boolean[][]{
+        B(1, 3, 0, new boolean[][]{
                 {false, true},
                 {true, true}
         }),
-        C(3, 1, 0, new Boolean[][]{
+        C(3, 1, 0, new boolean[][]{
                 {false, true},
                 {true, true}
         }),
-        D(2, 2, 0, new Boolean[][]{
+        D(2, 2, 0, new boolean[][]{
                 {true},
                 {true},
                 {true}
         }),
-        E(3, 2, 1, new Boolean[][]{
+        E(3, 2, 1, new boolean[][]{
                 {false, true},
                 {true, true},
                 {true, false}
         }),
-        F(2, 2, 0, new Boolean[][]{
+        F(2, 2, 0, new boolean[][]{
                 {true, false},
                 {true, true},
                 {true, true}
         }),
-        G(1, 4, 1, new Boolean[][]{
+        G(1, 4, 1, new boolean[][]{
                 {false, false, true, false, false},
                 {true, true, true, true, true},
                 {false, false, true, false, false}
         }),
-        H(0, 3, 1, new Boolean[][]{
+        H(0, 3, 1, new boolean[][]{
                 {false, true, false, false},
                 {true, true, true, false},
                 {false, true, false, false},
                 {false, true, false, false}
         }),
-        I(6, 5, 2, new Boolean[][]{
+        I(6, 5, 2, new boolean[][]{
                 {true, true},
                 {true, true}
         }),
-        J(4, 2, 0, new Boolean[][]{
+        J(4, 2, 0, new boolean[][]{
                 {true, false},
                 {true, true},
                 {true, true},
                 {false, true}
         }),
-        K(2, 2, 0, new Boolean[][]{
+        K(2, 2, 0, new boolean[][]{
                 {true, false},
                 {true, true},
                 {true, true}
         }),
-        L(1, 5, 1, new Boolean[][]{
+        L(1, 5, 1, new boolean[][]{
                 {true, true},
                 {false, true},
                 {true, true}
         }),
-        M(3, 3, 1, new Boolean[][]{
+        M(3, 3, 1, new boolean[][]{
                 {true},
                 {true},
                 {true},
                 {true}
         }),
-        N(7, 1, 1, new Boolean[][]{
+        N(7, 1, 1, new boolean[][]{
                 {true, true, true, true}
         }),
-        O(3, 4, 1, new Boolean[][]{
+        O(3, 4, 1, new boolean[][]{
                 {true, false},
                 {true, false},
                 {true, true},
                 {true, false}
         }),
-        P(7, 4, 2, new Boolean[][]{
+        P(7, 4, 2, new boolean[][]{
                 {true, false},
                 {true, true},
                 {true, true},
                 {true, false}
         }),
-        Q(3, 6, 2, new Boolean[][]{
+        Q(3, 6, 2, new boolean[][]{
                 {false, true, false},
                 {true, true, true},
                 {true, false, true}
         }),
-        R(2, 1, 0, new Boolean[][]{
+        R(2, 1, 0, new boolean[][]{
                 {false, true, false},
                 {false, true, true},
                 {true, true, false},
                 {false, true, false}
         }),
-        S(4, 6, 2, new Boolean[][]{
+        S(4, 6, 2, new boolean[][]{
                 {false, true},
                 {false, true},
                 {true, true}
         }),
-        T(5, 4, 2, new Boolean[][]{
+        T(5, 4, 2, new boolean[][]{
                 {false, true, false},
                 {true, true, true},
                 {false, true, false}
         }),
-        U(2, 3, 0, new Boolean[][]{
+        U(2, 3, 0, new boolean[][]{
                 {true, false, true},
                 {true, true, true},
                 {true, false, true}
         }),
-        V(5, 3, 1, new Boolean[][]{
+        V(5, 3, 1, new boolean[][]{
                 {false, true, false},
                 {true, true, true},
                 {true, true, true},
                 {false, true, false}
         }),
-        W(10, 3, 2, new Boolean[][]{
+        W(10, 3, 2, new boolean[][]{
                 {false, true},
                 {false, true},
                 {false, true},
                 {true, true}
         }),
-        X(5, 5, 2, new Boolean[][]{
+        X(5, 5, 2, new boolean[][]{
                 {false, true, false},
                 {false, true, false},
                 {true, true, true}
         }),
-        Y(10, 5, 3, new Boolean[][]{
+        Y(10, 5, 3, new boolean[][]{
                 {true, true},
                 {true, true},
                 {false, true},
                 {false, true}
         }),
-        Z(1, 2, 0, new Boolean[][]{
+        Z(1, 2, 0, new boolean[][]{
                 {true, true, false},
                 {false, true, false},
                 {false, true, false},
                 {false, true, true}
         }),
-        a(4, 2, 1, new Boolean[][]{
+        a(4, 2, 1, new boolean[][]{
                 {true, false},
                 {true, false},
                 {true, false},
                 {true, true}
         }),
-        b(7, 2, 2, new Boolean[][]{
+        b(7, 2, 2, new boolean[][]{
                 {false, true, false, false},
                 {false, true, false, false},
                 {false, true, false, false},
                 {true, true, true, false}
         }),
-        c(10, 4, 3, new Boolean[][]{
+        c(10, 4, 3, new boolean[][]{
                 {true, false, false},
                 {true, true, false},
                 {false, true, true}
         }),
-        d(1, 2, 0, new Boolean[][]{
+        d(1, 2, 0, new boolean[][]{
                 {true, false, true},
                 {true, true, true},
                 {false, false, false}
         }),
-        e(2, 3, 1, new Boolean[][]{
+        e(2, 3, 1, new boolean[][]{
                 {false, true},
                 {false, true},
                 {true, true},
                 {true, false}
         }),
-        f(7, 6, 3, new Boolean[][]{
+        f(7, 6, 3, new boolean[][]{
                 {false, true, true},
                 {true, true, false}
         }),
-        g(8, 6, 3, new Boolean[][]{
+        g(8, 6, 3, new boolean[][]{
                 {false, true, true},
                 {false, true, true},
                 {true, true, false}
         }),
-        h(0, 0, 0, new Boolean[][] {{true}});
+        h(0, 0, 0, new boolean[][] {{true}});
 
 
         private final int buttonCost;
         private final int timeCost;
         private final int buttonIncome;
-        private final Boolean[][] locationGrid;
+        private final boolean[][] locationGrid;
 
-        Patch(int buttonCost, int timeCost, int buttonIncome, Boolean[][] locationGrid) {
+        Patch(int buttonCost, int timeCost, int buttonIncome, boolean[][] locationGrid) {
             this.buttonCost = buttonCost;
             this.timeCost = timeCost;
             this.buttonIncome = buttonIncome;
@@ -203,7 +203,7 @@ public enum Patch {
             return buttonIncome;
         }
 
-        public Boolean[][] getLocationGrid() {
+        public boolean[][] getLocationGrid() {
                 return locationGrid;
         }
 }
